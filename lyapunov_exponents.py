55f71848d48b3f73e8bbf60e3342f146a61285ee
usbt0p <lucascdeaa@gmail.com>
2025-11-25 11:04:10 +0100

    numba optimize lyapunov computation

--- lyapunov_exponents.py
+++ lyapunov_exponents.py
@@ -1,10 +1,7 @@
-import argparse
 import os
 import json
 import glob
 import logging
-from multiprocessing import cpu_count
-from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import numba
 import math
@@ -19,7 +16,101 @@ from tqdm import tqdm
 from colors import (colorInterpolate, randomComplementaryColors, 
                     randomTriadColors, randomColoursGoldenRatio)
 
-def createAttractor(examples, iterations, out_path="output"):
+@numba.njit(fastmath=True)
+def compute_lyapunov(iterations, discard=1000):
+    """Compute the Lyapunov exponent of the attractor defined by coefficients a and b.
+    a and b are arrays of coefficients, x0 and y0 are the seed points.
+    discard: number of initial iterations to discard for transient behavior.
+    """
+    # random coefficients
+    ax = [random.uniform(-2, 2) for _ in range(6)]
+    ay = [random.uniform(-2, 2) for _ in range(6)]
+    # This is the initial point, it will serve as the seed for the series
+    # Even though the series is chaotic, it is deterministic
+    x = [random.uniform(-0.5, 0.5)]
+    y = [random.uniform(-0.5, 0.5)]
+    # min and max bounds
+    xmin = ymin = 1e32
+    xmax = ymax = -1e32
+    lyapunov = 0.0     
+    
+    # calculate the initial separation between two nearby points and ensure it's not zero
+    d0 = -1
+    while d0 <= 0: # until the points are not identical
+        # xe and ye are coords of our initial point, but slightly perturbed
+        xe = x[0] + random.uniform(-0.5, 0.5) / 1000.0
+        ye = y[0] + random.uniform(-0.5, 0.5) / 1000.0
+        # dx and dy aid us in later filtering point or infinite attractors,
+        # they are the vector between the seed and the perturbed point
+        dx = x[0] - xe
+        dy = y[0] - ye
+        # this is just the distance of the previous vector, 
+        # a theoretical infinitesimal that will be used to renormalize the
+        # distance between the two points after each iteration
+        d0 = math.sqrt(dx * dx + dy * dy) # (epsilon in Bennetin method)
+
+    drawit = True
+    # Calculate the attractor
+    for i in range(1, iterations): # start at 1 to avoid using x[-1]
+        # Calculate next term
+
+        x_i = ax[0] + ax[1]*x[i-1] + ax[2]*x[i-1]**2 + \
+                ax[3]*x[i-1]*y[i-1] + ax[4]*y[i-1] + ax[5]*y[i-1]**2
+        y_i = ay[0] + ay[1]*x[i-1] + ay[2]*x[i-1]**2 + \
+                ay[3]*x[i-1]*y[i-1] + ay[4]*y[i-1] + ay[5]*y[i-1]**2
+
+        x.append(x_i)
+        y.append(y_i)
+        # this represents a nearby point, but slightly separated, on another orbit
+        # we'll use this to calculate the lyapunov exponent
+        xenew = ax[0] + ax[1]*xe + ax[2]*xe**2 + ax[3]*xe*ye + ax[4]*ye + ax[5]*ye**2
+        yenew = ay[0] + ay[1]*xe + ay[2]*xe**2 + ay[3]*xe*ye + ay[4]*ye + ay[5]*ye**2
+
+        # Update the bounds
+        xmin = min(xmin, x[i])
+        ymin = min(ymin, y[i])
+        xmax = max(xmax, x[i])
+        ymax = max(ymax, y[i])
+
+        # Does the series tend to infinity
+        if xmin < -1e10 or ymin < -1e10 or xmax > 1e10 or ymax > 1e10:
+            drawit = False
+            #logging.info("infinite attractor")
+            break
+
+        # if the vector between current and prev point is too small, 
+        # the series will tend to a point
+        dx = x[i] - x[i - 1]
+        dy = y[i] - y[i - 1]
+        if abs(dx) < 1e-10 and abs(dy) < 1e-10:
+            drawit = False
+            #logging.info("point attractor")
+            break
+
+        # Calculate the lyapunov exponents using 
+        # we ignore the first iters to allow the series to settle down into a pattern
+        if i > discard:
+            # this approach of approximating the lyapunov exponent is called the Bennetin method
+            # it's not 100% equal to the mathematical definition, but it's a good approximation
+
+            # two vectors are computed as the difference between the current point and
+            # our perturbed point (xenew, yenew)
+            dx = x[i] - xenew
+            dy = y[i] - yenew
+            dd = math.sqrt(dx * dx + dy * dy) # compute the distance between the norm
+            # add the log of the ratio between the two current point's distances and the initial infinitesimal distance
+            lyapunov += math.log(math.fabs(dd / d0))
+            # renormalize the perturbed point to be d0 away from the current point (to avoid overflow or underflow of the value)
+            xe = x[i] + d0 * dx / dd
+            ye = y[i] + d0 * dy / dd
+
+        # TODO check for correctness of lyapunov normalization
+        # lyapunov /= (MAXITERATIONS - 1000) # normalize
+        # and what about T? 
+
+    return lyapunov, drawit, x, y, ax, ay, xmin, xmax, ymin, ymax
+
+def createAttractors(examples, iterations, out_path="output"):
     '''Generate strange attractors using random coefficients and save them as images and JSON files.
     If you want more insights into the algorithm: 
     https://pmc.ncbi.nlm.nih.gov/articles/PMC7512692/#sec3-entropy-20-00175
@@ -40,91 +131,9 @@ def createAttractor(examples, iterations, out_path="output"):
     try: # catch keyboard interrupt to save progress
         while file_index < examples:
 
-            # random coefficients
-            ax = [random.uniform(-2, 2) for _ in range(6)]
-            ay = [random.uniform(-2, 2) for _ in range(6)]
-            # This is the initial point, it will serve as the seed for the series
-            # Even though the series is chaotic, it is deterministic
-            x = [random.uniform(-0.5, 0.5)]
-            y = [random.uniform(-0.5, 0.5)]
-            # min and max bounds
-            xmin = ymin = 1e32
-            xmax = ymax = -1e32
-            lyapunov = 0.0     
-            
-            # calculate the initial separation between two nearby points and ensure it's not zero
-            d0 = -1
-            while d0 <= 0: # until the points are not identical
-                # xe and ye are coords of our initial point, but slightly perturbed
-                xe = x[0] + random.uniform(-0.5, 0.5) / 1000.0
-                ye = y[0] + random.uniform(-0.5, 0.5) / 1000.0
-                # dx and dy aid us in later filtering point or infinite attractors,
-                # they are the vector between the seed and the perturbed point
-                dx = x[0] - xe
-                dy = y[0] - ye
-                # this is just the distance of the previous vector, 
-                # a theoretical infinitesimal that will be used to renormalize the
-                # distance between the two points after each iteration
-                d0 = math.sqrt(dx * dx + dy * dy) # (epsilon in Bennetin method)
-
-            drawit = True
-            # Calculate the attractor
-            for i in range(1, iterations): # start at 1 to avoid using x[-1]
-                # Calculate next term
-
-                x_i = ax[0] + ax[1]*x[i-1] + ax[2]*x[i-1]**2 + \
-                        ax[3]*x[i-1]*y[i-1] + ax[4]*y[i-1] + ax[5]*y[i-1]**2
-                y_i = ay[0] + ay[1]*x[i-1] + ay[2]*x[i-1]**2 + \
-                        ay[3]*x[i-1]*y[i-1] + ay[4]*y[i-1] + ay[5]*y[i-1]**2
-
-                x.append(x_i)
-                y.append(y_i)
-                # this represents a nearby point, but slightly separated, on another orbit
-                # we'll use this to calculate the lyapunov exponent
-                xenew = ax[0] + ax[1]*xe + ax[2]*xe**2 + ax[3]*xe*ye + ax[4]*ye + ax[5]*ye**2
-                yenew = ay[0] + ay[1]*xe + ay[2]*xe**2 + ay[3]*xe*ye + ay[4]*ye + ay[5]*ye**2
-
-                # Update the bounds
-                xmin = min(xmin, x[i])
-                ymin = min(ymin, y[i])
-                xmax = max(xmax, x[i])
-                ymax = max(ymax, y[i])
-
-                # Does the series tend to infinity
-                if xmin < -1e10 or ymin < -1e10 or xmax > 1e10 or ymax > 1e10:
-                    drawit = False
-                    logging.info("infinite attractor")
-                    break
-
-                # if the vector between current and prev point is too small, 
-                # the series will tend to a point
-                dx = x[i] - x[i - 1]
-                dy = y[i] - y[i - 1]
-                if abs(dx) < 1e-10 and abs(dy) < 1e-10:
-                    drawit = False
-                    logging.info("point attractor")
-                    break
-
-                # Calculate the lyapunov exponents using 
-                # we ignore the first iters to allow the series to settle down into a pattern
-                if i > 1000:
-                    # this approach of approximating the lyapunov exponent is called the Bennetin method
-                    # it's not 100% equal to the mathematical definition, but it's a good approximation
-
-                    # two vectors are computed as the difference between the current point and
-                    # our perturbed point (xenew, yenew)
-                    dx = x[i] - xenew
-                    dy = y[i] - yenew
-                    dd = math.sqrt(dx * dx + dy * dy) # compute the distance between the norm
-                    # add the log of the ratio between the two current point's distances and the initial infinitesimal distance
-                    lyapunov += math.log(math.fabs(dd / d0))
-                    # renormalize the perturbed point to be d0 away from the current point (to avoid overflow or underflow of the value)
-                    xe = x[i] + d0 * dx / dd
-                    ye = y[i] + d0 * dy / dd
-
-                # TODO check for correctness of lyapunov normalization
-                # lyapunov /= (MAXITERATIONS - 1000) # normalize
-                # and what about T? 
+            lyapunov, drawit, x, y, ax, ay, xmin, xmax, ymin, ymax = compute_lyapunov(
+                iterations=iterations, discard=1000,
+            )
 
             # Classify the series according to lyapunov
             if drawit:
@@ -377,7 +386,7 @@ if __name__ == "__main__":
     EXAMPLES = 100
     RENDER_ITERATIONS = 25_000_000 # long processing but good rendering
 
-    base_dir = "lucas_laura"
+    base_dir = "numba"
 
     attractors = [4750, 4755, 4761, 4827, 4866, 4894, 4909, 4917, 4944, 
                   4979, 5009, 5014, 5043, 5059, 5061, 5072, 5094, 5095, 
@@ -398,7 +407,7 @@ if __name__ == "__main__":
     
     # TODO parallelize this too
     # find EXAMPLES random attractors with CREATE_ITERATIONS iters for each one
-    #createAttractor(out_path=f"{base_dir}/out", examples=EXAMPLES, iterations=CREATE_ITERATIONS)
+    createAttractors(out_path=f"{base_dir}/out", examples=EXAMPLES, iterations=CREATE_ITERATIONS)
 
     recolor = {'n':1, 'color':(0,0,0), 'resample_size':512}
 
@@ -445,7 +454,7 @@ if __name__ == "__main__":
         results = draw_attractors_in_parallel(
             input_files, 
             RENDER_ITERATIONS, 
-            n_processes=cpu_count() - 6, # leave some CPU for other tasks
+            n_processes=os.cpu_count() - 6, # leave some CPU for other tasks
             batch_size=10,
             **render_kwargs
         )
